6bed8ea9385157d2569a8a7bb95cd219ba78e403
hyoyh <hyosunnyaya@gmail.com>
2025-07-19 17:21:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,7 @@
 const CACHE_NAME = 'sodam-seoul-v1';
 const urlsToCache = [
   './',
-  './index.html',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://cdn.tailwindcss.com',
-  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js'
+  './index.html'
 ];
 
 self.addEventListener('install', event => {
